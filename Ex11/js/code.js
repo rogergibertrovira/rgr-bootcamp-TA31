88c180f8d539c658d43055ba61723c0efa64a0bf
Roger <49531890+rogergibertrovira@users.noreply.github.com>
2023-05-03 10:32:11 +0200

    Ex11

---
+++ Ex11/js/code.js
@@ -0,0 +1,21 @@
+let tiradas = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+const tirarDados = () => {
+  let min = Math.ceil(1);
+  let max = Math.floor(6);
+  let dado1 = Math.floor(Math.random() * (max - min + 1) + min);
+  let dado2 = Math.floor(Math.random() * (max - min + 1) + min);
+
+  let suma = dado1 + dado2;
+
+  tiradas[suma]++;
+};
+
+for (i = 0; i < 10000; i++) {
+  tirarDados();
+}
+
+console.log("Sumas de los lanzamientos");
+for (i = 2; i <= 12; i++) {
+  console.log(i + ": " + tiradas[i]);
+}
